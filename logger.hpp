c827b4c4327f9cf50144cac7e637398db813c0bb
acedev003 <acedeveloper003@gmail.com>
2022-12-27 12:52:55 +0530

    Base class declaration

--- logger.hpp
+++ logger.hpp
@@ -1,8 +1,76 @@
 #pragma once
 
-enum LogLevels { FATAL, ERROR, WARN, INFO, DEBUG };
+#include<string>
+#include<iostream>
 
-class Logging
+//enum LogLevels  { FATAL, ERROR, WARN, INFO, DEBUG };
+
+class Logger
 {
-    
+    public:
+        static void init(bool console_output = true,bool save_to_file = false,std::string log_file_path = "")
+        {
+            if(console_output == false && save_to_file == false)
+            {
+                std::cerr<<"Both file and console outputs are disabled. Failed to initialize logger\n";
+                return;
+            }
+
+            if(save_to_file)
+            {
+                if(log_file_path != "")
+                {
+                    get_instance().log_file_path = log_file_path;
+                }
+            }
+            else
+            {
+                //TODO
+            }
+            
+            get_instance().initialized = true;
+            std::cout<<get_instance().log_file_path;
+            
+        }
+
+        static void Fatal()
+        {
+            
+        }
+
+        static void Error()
+        {
+            
+        }
+
+        static void Warn()
+        {
+            
+        }
+
+        static void Info()
+        {
+            
+        }
+
+        static void Debug()
+        {
+            
+        }
+
+    private:  
+        static bool initialized;
+        static std::string log_file_path;
+
+        Logger(){}
+
+        static Logger get_instance()
+        {
+            static Logger instance;
+            return instance;
+        }
+
 };
+
+bool Logger::initialized  = false;
+std::string Logger::log_file_path = "LOG.txt";
