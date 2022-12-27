dd03c123408b7c9bc58296570f0c0ed7304b6fa5
acedev003 <acedeveloper003@gmail.com>
2022-12-27 19:38:45 +0530

    Improved code functionality

--- logger.hpp
+++ logger.hpp
@@ -1,46 +1,50 @@
 #pragma once
 
+#include<chrono>
 #include<string>
 #include<iostream>
 
-//enum LogLevels  { FATAL, ERROR, WARN, INFO, DEBUG };
+enum LogLevel  { FATAL = 4, ERROR = 3, WARN = 2, INFO = 1, DEBUG = 0 };
 
 class Logger
 {
     public:
-        static void init(bool console_output = true,bool save_to_file = false,std::string log_file_path = "")
+        static void init(LogLevel priority_level = INFO,bool save_to_file = false,bool console_output = true,std::string log_file_path = "")
         {
             if(console_output == false && save_to_file == false)
             {
-                std::cerr<<"Both file and console outputs are disabled. Failed to initialize logger\n";
+                //Both console and file outputs disabled. Exiting logger;
                 return;
             }
 
             if(save_to_file)
             {
+                // Logging to file enabled
                 if(log_file_path != "")
                 {
                     get_instance().log_file_path = log_file_path;
                 }
             }
-            else
+            
+            if(console_output)
             {
-                //TODO
+                //Logging to console enabled
             }
+
             
-            get_instance().initialized = true;
-            std::cout<<get_instance().log_file_path;
-            
+            get_instance().priority_level = priority_level;
+            get_instance().initialized    = true;
         }
 
-        static void Fatal()
+        template<typename ... Args>
+        static void Fatal(std::string message,Args ... args)
         {
-            
+            log(LogLevel::FATAL,message+"\n",args...);
         }
 
         static void Error()
         {
-            
+               
         }
 
         static void Warn()
@@ -58,8 +62,11 @@ class Logger
             
         }
 
-    private:  
+    private:
         static bool initialized;
+        static bool console_output;
+        static bool save_to_file;
+        static LogLevel priority_level;
         static std::string log_file_path;
 
         Logger(){}
@@ -69,8 +76,28 @@ class Logger
             static Logger instance;
             return instance;
         }
+        
+        template<typename... Args>
+        static void log(LogLevel log_level,std::string message,Args ... args)
+        {
+            if(log_level >= get_instance().priority_level && get_instance().initialized)
+            {
+                if(get_instance().console_output)
+                {
+                    printf(message.c_str(),args...);   
+                }
+
+                if(get_instance().save_to_file)
+                {
+
+                }
+            }
+        }
 
 };
 
-bool Logger::initialized  = false;
-std::string Logger::log_file_path = "LOG.txt";
+bool Logger::initialized     = false;
+bool Logger::console_output  = true;
+bool Logger::save_to_file    = false;
+LogLevel    Logger::priority_level = LogLevel::INFO;
+std::string Logger::log_file_path  = "LOG.txt";
