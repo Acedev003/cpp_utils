b60e246be70764f3c435b6990e9bb20cbfa95b06
acedev003 <acedeveloper003@gmail.com>
2023-01-07 19:02:46 +0530

    Shift away from singleton pattern to regular OOP

--- ace_logger.hpp
+++ ace_logger.hpp
@@ -12,48 +12,31 @@ enum class LogLevel : std::uint8_t  { DEBUG, INFO, WARN, ERROR, FATAL };
 class Logger
 {
     public:
-        static void init(LogLevel priority_level = LogLevel::INFO,bool save_to_file = false,bool console_output = true,std::string log_file_path = "");
+        Logger(LogLevel priority_level = LogLevel::INFO,std::string log_file_path = "",bool console_output = true);
 
-        static void Fatal(const std::string& message);
-        static void Error(const std::string& message);
-        static void Warn (const std::string& message);
-        static void Info (const std::string& message);
-        static void Debug(const std::string& message);
+        void Fatal(const std::string& message);
+        void Error(const std::string& message);
+        void Warn (const std::string& message);
+        void Info (const std::string& message);
+        void Debug(const std::string& message);
 
     private:
-        static bool initialized;
-        static bool console_output;
-        static bool save_to_file;
-        static LogLevel    priority_level;
-        static std::string log_file_path;
-        static std::mutex  logger_mutex; 
+        bool initialized    = false;
+        bool console_output = true;
+        bool save_to_file   = false;
+        LogLevel    priority_level = LogLevel::INFO;
+        std::string log_file_path  = "LOG.txt";
+        std::mutex  logger_mutex; 
 
-        Logger(){}
-        
-        static Logger get_instance();
-        static void log(LogLevel log_level,const std::string& message);
+        void log(LogLevel log_level,const std::string& message);
 };
 
 #ifdef ACE_LOGGER_IMPLEMENTATION
 
-bool Logger::initialized     = false;
-bool Logger::console_output  = true;
-bool Logger::save_to_file    = false;
-LogLevel    Logger::priority_level = LogLevel::INFO;
-std::string Logger::log_file_path  = "LOG.txt";
-std::mutex  Logger::logger_mutex;
-
-Logger Logger::get_instance()
-{
-    static Logger instance;
-    return instance;
-}
-
-void Logger::init(LogLevel priority_level,bool save_to_file,bool console_output,std::string log_file_path)
+Logger::Logger(LogLevel priority_level,std::string log_file_path,bool console_output)
 {
     if(!initialized)
     {
-        Logger this_logger = get_instance();
         if (console_output == false && save_to_file == false)
         {
             // Both console and file outputs disabled. Exiting logger;
@@ -65,22 +48,20 @@ void Logger::init(LogLevel priority_level,bool save_to_file,bool console_output,
             // Logging to file enabled
             if (log_file_path != "")
             {
-                this_logger.log_file_path = log_file_path;
+                this->log_file_path = log_file_path;
             }
-            this_logger.save_to_file = true;
+            this->save_to_file = true;
         }
 
-        this_logger.console_output = console_output;
-        this_logger.priority_level = priority_level;
-        this_logger.initialized = true;
+        this->console_output = console_output;
+        this->priority_level = priority_level;
+        this->initialized = true;
     }
 }
 
 void Logger::log(LogLevel log_level,const std::string& message)
 {
-    Logger this_logger = get_instance();
-
-    if (log_level >= this_logger.priority_level && this_logger.initialized)
+    if (log_level >= this->priority_level && this->initialized)
     {
         logger_mutex.lock();
         bool time_format_avail = false;
@@ -98,7 +79,7 @@ void Logger::log(LogLevel log_level,const std::string& message)
             time_format_avail = true;
         }
 
-        if (this_logger.console_output)
+        if (this->console_output)
         {
             if (time_format_avail)
             {
@@ -107,7 +88,7 @@ void Logger::log(LogLevel log_level,const std::string& message)
             std::cout << message << std::endl;
         }
 
-        if (this_logger.save_to_file)
+        if (this->save_to_file)
         {
             std::ofstream file(log_file_path, std::ios_base::app);
             if (time_format_avail)
