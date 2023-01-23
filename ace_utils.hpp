bc967884e0b9e14197efce98d07e0a6cb130a496
acedev003 <acedeveloper003@gmail.com>
2023-01-23 22:08:41 +0530

    Add python print like function

--- ace_utils.hpp
+++ ace_utils.hpp
@@ -1,9 +1,17 @@
 #ifndef ACE_UTILS_HEADER
 #define ACE_UTILS_HEADER
 
+#include <iostream>
+#include <string>
+
+void print(const std::string& message);
 
 #ifdef ACE_UTILS_IMPLEMENTATION
 
+void print(const std::string& message)
+{
+    std::cout<<message<<"\n";
+}
 
 #endif
 #endif
